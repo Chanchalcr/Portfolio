0fce2800e26195844c6c1bba950666d521c67c4d
Chanchalcr <chanchalrai193@gmail.com>
2022-07-23 14:45:37 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div className="App">
-      This is the start of my portfolio website!! and I and super excited!! yall!!!
+      This is the start of my portfolio website!! and I and super excited!! yall!!! <br/>
+      I want to tell you something... Everything will be alright! You are so cool! And you don't have to worry about anything!
     </div>
   );
 }
