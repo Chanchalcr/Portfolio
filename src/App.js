c3e41235179e5d605fe798062e20079ac0507de3
Chanchalcr <chanchalrai193@gmail.com>
2022-07-23 14:13:17 +0530

    This is the first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      This is the start of my portfolio website!! and I and super excited!! yall!!!
+    </div>
+  );
+}
+
+export default App;
