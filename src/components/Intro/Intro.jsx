cebb65569459f8e06dffbbeb5a5378f713f97a03
Chanchalcr <chanchalrai193@gmail.com>
2022-07-24 15:47:07 +0530

    Added image

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
-import './Intro.css'
-
+import React from "react";
+import "./Intro.css";
+// import Typewriter from "typewriter-effect";
+import Github from '../../img/github.png'
+import LinkedIn from '../../img/linkedin.png'
+import Instagram from '../../img/instagram.png'
+import Chanchal from '../../img/chanchal.png'
+import Vector1 from '../../img/Vector1.png'
+import Vector2 from '../../img/Vector2.png'
 export default function Intro() {
   return (
-    <div className='intro'>
+    <div className="intro">
       <div className="i-left">
         <div className="i-name">
-            
+          {/* <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+              .typeString("Hi! I'm ")
+              .start();
+            }}
+          /> */}
+          <span>Hi! I'm </span>
+          <span>Chanchal</span>
+          <span>I am an engineering student and a tech enthusiast. I love full-stack web development and coding Java!</span>
+        </div>
+        <button className="button i-button">Hire me</button>
+        <div className="i-icons">
+            <a href="https://github.com/Chanchalcr">
+            <img src={Github} alt=""/>
+            </a>
+            <a href="https://www.linkedin.com/in/chanchal-rai-4456b2216/">
+            <img src={LinkedIn} alt=""/>
+            </a>
+            <a href="https://www.instagram.com/chanchal.rai.8/?hl=en">
+            <img src={Instagram} alt=""/>
+            </a>
         </div>
       </div>
-      <div className="iright"></div>
+      <div className="i-right">
+        <img src={Chanchal} alt="" width={500}/>
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+      </div>
     </div>
-  )
+  );
 }
