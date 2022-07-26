3ba4ebea7e5dd3fd35a41954f065944a99f057c0
Chanchalcr <chanchalrai193@gmail.com>
2022-07-26 23:34:10 +0530

    added floating divs

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -7,6 +7,10 @@ import Instagram from '../../img/instagram.png'
 import Chanchal from '../../img/chanchal.png'
 import Vector1 from '../../img/Vector1.png'
 import Vector2 from '../../img/Vector2.png'
+import thumbup from '../../img/thumbup.png'
+import crown from '../../img/crown.png'
+import glassesimoji from '../../img/glassesimoji.png'
+import FlotingDiv from "../FlotingDiv/FlotingDiv";
 export default function Intro() {
   return (
     <div className="intro">
@@ -39,6 +43,21 @@ export default function Intro() {
         <img src={Chanchal} alt="" width={500}/>
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
+        <div className="imogi">
+        <img src={glassesimoji} alt=""/>
+        </div>
+        <div style={{top:"64%", left: "0%", zIndex:'2'}}>
+          <FlotingDiv image={crown} txt1='Web' txt2='Developer'/>
+        </div>
+        <div style={{top:"38%", left: "77%", zIndex: '2'}}>
+          <FlotingDiv image={thumbup} txt1='Data' txt2='Structures'/>
+        </div>
+        <div style={{top:"64%", left: "70%", zIndex: '2'}}>
+          <FlotingDiv image={thumbup} txt1='Algorithms' txt2=''/>
+        </div>
+        <div className="blur" style={{background: "rgb(238 210 255)"}}>
+        </div>
+        <div className="blur" style={{background:'#C1F5FF', top:'17rem', width:'21rem', height:'11rem', left: '-9rem'}}></div>
       </div>
     </div>
   );
